68aa2c5f2c66ee471ae01eaf10d26e2cb894a2f2
Mahesh Prasad <mahesh.prasad@baramundi.com>
2024-05-03 16:09:33 +0200

    add infrastructure

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type        = string
+  description = "The environment name"
+}
+
+variable "location" {
+  type        = string
+  description = "The resource location"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "The storage account name"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
